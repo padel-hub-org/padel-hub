e4c29e76e3c74fbc73e50ae7455c0e349bca2267
armanpersonal <arman.noor3@gmail.com>
2025-10-19 19:49:53 +0200

    setting up inertia and svelte

---
+++ resources/js/Pages/Home.svelte
@@ -0,0 +1,5 @@
+<script>
+  export let name = 'World'
+</script>
+
+<h1>Hello {name} from Svelte + Inertia!</h1>
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome', [
-        "name" => "padel masters"
-    ]);
+    return Inertia::render('Home', ['name' => 'Developer']);
 });
