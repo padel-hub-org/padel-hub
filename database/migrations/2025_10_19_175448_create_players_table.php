8691d178478ac4fa1c801c675d088dc81b01dddd
armanpersonal <arman.noor3@gmail.com>
2025-10-19 21:52:13 +0200

    fix type error in migration

--- database/migrations/2025_10_19_175448_create_players_table.php
+++ database/migrations/2025_10_19_175448_create_players_table.php
@@ -5,8 +5,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class() extends Migration {
     /**
      * Run the migrations.
      */
@@ -16,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->foreignIdFor(User::class);
-            $table->int('rating');
+            $table->integer('rating');
             $table->timestamps();
         });
     }
