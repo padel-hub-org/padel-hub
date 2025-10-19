ae1b07001564720526e9e512a0ca8895d84570db
armanpersonal <arman.noor3@gmail.com>
2025-10-19 21:15:20 +0200

    added Player table with relationship to user

---
+++ app/Models/Player.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Player extends Model
+{
+    /** @use HasFactory<\Database\Factories\PlayerFactory> */
+    use HasFactory;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
