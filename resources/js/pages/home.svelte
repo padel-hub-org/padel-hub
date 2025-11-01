151d4d13a3a00ec98891069548c646e47a7b7905
Bjornar97 <bjornar.h+github@hey.com>
2025-11-01 18:41:44 +0000

    Use wayfinder directly in button component

--- resources/js/pages/home.svelte
+++ resources/js/pages/home.svelte
@@ -4,7 +4,6 @@
     import { Button } from "@/lib/components/ui/button";
     import { logout } from "@/routes";
     import type { User } from "@/types/User";
-    import { router } from "@inertiajs/svelte";
 
     interface Props {
         user?: User;
@@ -18,8 +17,8 @@
 
     {#if user}
         <Button href="/players">Go to Players</Button>
-        <Button onclick={() => router.post(logout())}>Log out</Button>
+        <Button href={logout()}>Log out</Button>
     {:else}
-        <Button href={redirectToGoogle().url}>Log in with Google</Button>
+        <Button href={redirectToGoogle()}>Log in with Google</Button>
     {/if}
 </div>
