e60fdf20d2fd8c446b999f7dc476cda77e997f89
Arman Noor <99976902+armanpersonal@users.noreply.github.com>
2026-01-04 22:14:28 +0100

    *Added new EventPlayer controller for components and data related to event players (#47)
    *Updated css for event-players.svelte to make it more streamlined with events/show
    *Added new index.svelte file for event players to add and remove players from an event
    
    Co-authored-by: Arman <arman.noor3gmail.com>

--- resources/js/actions/App/Http/Controllers/index.ts
+++ resources/js/actions/App/Http/Controllers/index.ts
@@ -2,12 +2,14 @@ import HomeController from './HomeController'
 import AuthController from './AuthController'
 import PlayerController from './PlayerController'
 import EventController from './EventController'
+import EventPlayerController from './EventPlayerController'
 
 const Controllers = {
     HomeController: Object.assign(HomeController, HomeController),
     AuthController: Object.assign(AuthController, AuthController),
     PlayerController: Object.assign(PlayerController, PlayerController),
     EventController: Object.assign(EventController, EventController),
+    EventPlayerController: Object.assign(EventPlayerController, EventPlayerController),
 }
 
 export default Controllers
