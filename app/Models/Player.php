cd78b7cb67ce54a60bf1abc65df7882b19488e0a
Bjornar97 <bjornar.h+github@hey.com>
2026-01-10 16:43:28 +0000

    Add soft delete to player model

--- app/Models/Player.php
+++ app/Models/Player.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * @property int $id
@@ -37,6 +38,8 @@ class Player extends Model
 {
     /** @use HasFactory<\Database\Factories\PlayerFactory> */
     use HasFactory;
+
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
