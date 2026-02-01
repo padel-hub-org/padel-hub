a0b3ee5305be965731da57c22ee51758b32255c2
Bjornar97 <bjornar.h+github@hey.com>
2026-02-01 14:41:08 +0000

    Reduce queries when calculating event ratings

--- app/Services/RatingService.php
+++ app/Services/RatingService.php
@@ -3,8 +3,10 @@
 namespace App\Services;
 
 use App\Models\Event;
+use App\Models\EventPlayer;
 use App\Models\Player;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Relations\Relation;
 
 class RatingService
 {
@@ -23,21 +25,46 @@ class RatingService
      */
     public function calculateEventRatings(Collection $players): void
     {
+        $players->load([
+            'gamePlayers' => function (Relation $query) {
+                $query->whereHas('game', fn ($q) => $q->where('event_id', $this->event->id))
+                    ->with('game.gamePlayers');
+            },
+        ]);
+
+        $eventPlayerRatings = EventPlayer::query()
+            ->where('event_id', $this->event->id)
+            ->pluck('start_rating', 'player_id')
+            ->toArray();
+
         foreach ($players as $player) {
-            $gamePlayers = $player->gamePlayers()->whereRelation('game', 'event_id', $this->event->id)->get();
+            $gamePlayers = $player->gamePlayers;
+            $playerStartRating = $eventPlayerRatings[$player->id] ?? 0;
 
             $sumOfPersonalPr = 0;
 
-            // TODO: try to reduce number of queries done in this forloop particularly
-            // regarding the attributes in gameplayer.
             foreach ($gamePlayers as $gamePlayer) {
+                $opponents = $gamePlayer->game->gamePlayers
+                    ->where('player_id', '!=', $gamePlayer->player_id)
+                    ->where('player_id', '!=', $gamePlayer->partner_id);
+
+                $opponentPoints = $opponents->first()?->points;
+
+                $opponentPlayerIds = $opponents->pluck('player_id');
+                $avgOpponentRating = (int) round(
+                    $opponentPlayerIds->map(fn ($id) => $eventPlayerRatings[$id] ?? 0)
+                        ->avg()
+                );
+
+                $partnerRating = $eventPlayerRatings[$gamePlayer->partner_id] ?? null;
+
                 $calculator = new CalculationService;
 
                 $calculator
                     ->withResult($gamePlayer->result)
-                    ->withPoints($gamePlayer->points, $gamePlayer->opponent_points)
-                    ->withAvgOpponentRating($gamePlayer->average_opponent_rating)
-                    ->withRatings($player->pivot->start_rating, $gamePlayer->partner_rating);
+                    ->withPoints($gamePlayer->points, $opponentPoints)
+                    ->withAvgOpponentRating($avgOpponentRating)
+                    ->withRatings($playerStartRating, $partnerRating);
 
                 $sumOfPersonalPr += $calculator->getPersonalPr();
             }
